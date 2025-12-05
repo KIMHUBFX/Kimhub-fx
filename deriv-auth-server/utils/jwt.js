848c2eb071f92e1228347c5297029e46ae13eace
KIMHUBFX <kimutaicollins027@gmail.com>
2025-12-05 18:56:55 +0300

    Update jwt.js

--- deriv-auth-server/utils/jwt.js
+++ deriv-auth-server/utils/jwt.js
@@ -4,7 +4,13 @@ dotenv.config();
 
 export const generateToken = (user) => {
   return jwt.sign(
-    { id: user._id, derivId: user.derivId, name: user.name, email: user.email, referral: user.referral },
+    {
+      id: user._id,
+      derivId: user.derivId,
+      name: user.name,
+      email: user.email,
+      referral: user.referral
+    },
     process.env.JWT_SECRET,
     { expiresIn: '1h' }
   );
