2325e89eab606f909cf9e463908de13f541b18f9
KIMHUBFX <kimutaicollins027@gmail.com>
2025-12-05 18:55:14 +0300

    Update user.js

--- deriv-auth-server/models/user.js
+++ deriv-auth-server/models/user.js
@@ -1,11 +1,22 @@
 import mongoose from 'mongoose';
 
+const tradeSchema = new mongoose.Schema({
+  trade_id: String,
+  symbol: String,
+  amount: Number,
+  profit: Number,
+  timestamp: Date
+}, { _id: false });
+
 const userSchema = new mongoose.Schema({
   derivId: { type: String, required: true, unique: true },
   name: String,
   email: String,
   balance: Number,
-  referral: String
+  referral: String,
+  referralCount: { type: Number, default: 0 },
+  referredUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  trades: [tradeSchema],
 }, { timestamps: true });
 
 export default mongoose.model('User', userSchema);
